00b942187c087921ca23a518e85f4b3d05cf4b48
quietCoder500 <raspberrypisms1@gmail.com>
2025-01-20 20:07:00 -0500

    Base setup

---
+++ controler.py
@@ -0,0 +1,34 @@
+from buildhat import MotorPair
+
+pair = MotorPair('C', 'D')
+
+def Init():
+    # Starts the timer
+    pass
+
+def Right():
+    pass
+
+def Left():
+    pass
+
+def Forward():
+    pass
+
+def Backward():
+    pass
+
+def Rotate(degrees):
+    pass
+
+def Stop():
+    pass
+
+def Finish():
+    pass
+
+def FrontAlign():
+    pass
+
+def YawAlign():
+    pass
